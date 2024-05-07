b053ff048d85372f5ef1a851111580a4b0767cb1
prao <priyankaprakash.rao@Mail.utoronto.ca>
2024-05-07 19:37:57 -0400

    finished updating the readme

--- main.py
+++ main.py
@@ -12,12 +12,10 @@ def main():
     
     robot1 = mod_cr.Robot(6e-3, 30) #setting dimensions of the robot with radius (in m) and number of disks
 
-   # calculating initial configuration
-   # A node is used to identify a configuration of the robot with tendon length and segment length, with the robot being able to change both of them
-   # The robot has two degrees of freedom : 1 from length insertion and 1 from bending due to change in tendon length at [r_disk, 0, 0]
+    # calculating initial configuration
+
     config_init = Node(robot1, 0.001, 0.001)
     config_init.set_init_guess(np.array([1]*robot1.nd)) 
-    config_init.T = np.eye(4)
 
     #running a 2D forward model to calculate a configuration 
     # model types = [KINEMATIC_CPP or KINEMATIC_MATLAB]
